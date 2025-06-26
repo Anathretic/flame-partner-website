b66b4afb36a67cafbfe0164d325705a749e5d4b0
Konrad <konrad.wojtylo.9@gmail.com>
2025-06-26 19:26:02 +0200

    Added scrollToTop do recruitment special btn

--- src/components/RecruitmentSpecialBtn/RecruitmentSpecialBtn.tsx
+++ src/components/RecruitmentSpecialBtn/RecruitmentSpecialBtn.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import { scrollToTop } from '../../utils/scrollToTopUtils';
 
 import styles from './styles/styles.module.scss';
 
 const RecruitmentSpecialBtn: React.FC = () => {
 	return (
-		<Link to='/rekrutacja-kierowcow' className={styles['recruitment-special-btn']}>
+		<Link to='/rekrutacja-kierowcow' className={styles['recruitment-special-btn']} onClick={scrollToTop}>
 			Aplikuj
 		</Link>
 	);
